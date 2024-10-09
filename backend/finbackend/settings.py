c894b5c6d3fe54bf5efa7014a07bf9759371e4fa
Cole Palmore <colepalmore2@gmail.com>
2024-10-09 09:38:17 -0500

    added landing page to server

--- backend/finbackend/settings.py
+++ backend/finbackend/settings.py
@@ -39,6 +39,7 @@ ALLOWED_HOSTS = ['0.0.0.0']
 # Application definition
 
 INSTALLED_APPS = [
+    'landingPage.apps.LandingpageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -128,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
