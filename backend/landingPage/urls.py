12b8b18cf39aa60f07b869b73ac688412ef58295
Cole Palmore <50222385+colepp@users.noreply.github.com>
2024-10-24 17:38:21 -0500

    Delete backend directory Siwtching to node.js server

--- backend/landingPage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-# import the path function for linking the view to the path
-# import the views for the path function
-
-# NOTE should add another page for login succes and login semi-succes(as in no attached bank account)
-urlpatterns = [
-    path("",views.index,name='index')
-]
--- backend/signup/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def signup(request):
-    return render(request,'signup/index.html')
