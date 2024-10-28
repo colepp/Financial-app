c0317aad6c63fc053ebc05f140c871b06585bd01
Cole Palmore <colepalmore2@gmail.com>
2024-10-28 09:47:08 -0500

    created signup login functions with hashin

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,16 @@
+// FUTURE GOALS
+/*
+#. goal ~ priority (status)
+1. Set up https certs for server ~ low (not started)
+2. secure signup process ~ high (working on)
+3. create login process and secure it ~ high (working on)
+4. create function for password reset ~ med (not started)
+5. set up cookie and session storage ~ high/med (started not working on)
+6. find code repeated code that can be turned into functions/objects ~ low (working on)
+7. move server functions and or page request to their own files....? ~ low (not started)
+*/
+
+
 
 //init path
 const path = require('path'); // for linking static and html files
@@ -6,14 +19,15 @@ const path = require('path'); // for linking static and html files
 const express = require('express'); // express server framework
 const bodyParser = require('body-parser'); // parser for middleware
 
-// init helmet secrutity for middleware and server
-const helmet = require('helmet');
+// init Security
+const helmet = require('helmet'); //helmet secrutity for middleware and server
+const bcrypt = require('bcrypt'); // password hashing
 
 // const exp = require('constants');
 const pool = require('./db'); // database access
 
 // init app
-const app = express();
+const app = express(); // express app object
 
 
 // init express session manager
@@ -25,17 +39,20 @@ app.use(
         secret:'Q+-k"]#@@4}*z<@2empu7dn~Jn.T#;', // session object key put this in .env file
         resave:false, // toggler for resaving the session into stoarge if the page has been modified or not
         saveUninitialized:false, // toggler for saving unitialized session
+        cookie:{
+            secure:false,
+            httpOnly:true,
+            maxAge:1000 * 60 * 60 * 24, // age of session item (1 whole day in milliseconds)
 
+        },
 
     })
-)
-
-
+);
 
 
 // Middleware
-app.use(helmet());
-app.use(bodyParser.json());
+app.use(helmet()); 
+app.use(bodyParser.json()); // parse json request
 
 
 // active port being used
@@ -70,21 +87,43 @@ app.get('/signup',(req,res) => {
 });
 
 app.post('/signup',async (req,res)=> {
+    // gather signup items
     const {email,first_name,last_name,password,phone_number,} = req.body;
 
-    try {
-    // Attempting to insert user data into the data base return 201 if it succeeds return 500
-        const result = await pool.query(
-            'INSERT INTO users (email, first_name, last_name, password, phone_number) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-            [email, first_name, last_name, password, phone_number]
-        );
-        const new_user = result.rows[0];
-        res.status(201).json(new_user);
-        console.log('user created successfully');
-    }catch (error){
-        console.error(error);
-        res.status(500).json({error:'User Could not be made'});
+    // check if email already in use if so return error 
+    const user_exist = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+    if(user_exist.rows.length > 0){
+        return res.status(409).send('Email Already Taken.');
     }
+
+    // hash passowrd
+    const hashing_rounds = 10;
+    const hashed_password = bcrypt.hash(password,hashing_rounds);
+
+    // try to append new user with hashed password if not then return error 500
+    try{
+        await pool.query('INSERT INTO users (email,first_name,last_name,phone_number,password) VALUES ($1,$2,$3,$4,$5)',[email,first_name,last_name,phone_number,hashed_password]);
+        res.status(201).send('User Registered')
+    }catch(error){
+        console.log('Error Registering Users',error);
+        res.status(500).send('User Could Not Be Registered');
+    }
+
+});
+
+// Login Page
+
+app.post('/login',async (req,res) => {
+    const {email,password} = req.body;
+
+    // check if user exists
+    const user_exist = await pool.query('SELECT * FROM users WHERE email = $1',[email]);
+    if(user_exist.rows.length === 0){
+        return res.status(401).send('Invalid Email or Password.');
+    }
+
+    // password compare
+    const password_confirm = await bcrypt.compare(password,user_exist.rows[0].password)
 });
 
 
@@ -94,7 +133,7 @@ app.get('/Settings',(req,res) => {
     res.sendFile(path.join(__dirname,STATIC_ROUTE,'Settings Page','index.html'));
 });
 
-
+// set server to listen on selected port
 app.listen(port,() =>{
-    console.log(`web app listening on ${port}`);
+    console.log(`"Server is running on http://localhost:${port}`);
 });
