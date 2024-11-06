b25ba7dbfb652ace7eb9b1fb9416e492bf235566
Cole Palmore <colepalmore2@gmail.com>
2024-11-06 09:08:48 -0600

    updated server file

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,8 @@
 */
 
 
+//init https
+const https = require('https');
 
 //init path
 const path = require('path'); // for linking static and html files
@@ -61,6 +63,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // active port being used
 const port = 3000;
+const api_port = 8000;
 
 
 
@@ -133,15 +136,15 @@ app.post('/signup',async (req,res)=> {
 
 });
 
-function getAcessToken(){
+async function getAcessToken(){
 
     return null;
 }
 
 // Plaid Stuff
 
-app.get('/register',(req,res)=>{
-
+app.get('/register',async (req,res)=>{
+    res.sendFile(path.join(__dirname,STATIC_ROUTE,'Register','index.html'));
 });
 
 
@@ -182,3 +185,96 @@ app.get('/Settings',(req,res) => {
 app.listen(port,() => {
     console.log(`"Server is running on http://localhost:${port}`);
 });
+
+
+
+// API
+
+const { Configuration, PlaidApi, PlaidEnvironments } = require('plaid');
+
+
+// Config for the plaid enviornment
+
+const config = new Configuration({
+    basePath: PlaidEnvironments[process.env.PLAID_ENV],
+    baseOptions: {
+        headers:{
+            'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
+            'PLAID-SECRET': process.env.PLAID_SECRET,
+            'Plaid-Version': '2020-09-14',
+        }
+    }
+});
+
+const client = new PlaidApi(config); // Plaid client object
+
+/*generate a link_token, which is a short-lived, 
+one-time use token required to initialize Plaid Link.*/
+app.get("/api/create_link_token", async (req, res, next) => {
+    const tokenResponse = await client.linkTokenCreate({
+      user: { client_user_id: req.sessionID },
+      client_name: "WealthWise",
+      language: "en",
+      products: ["transactions"],
+      country_codes: ["US"],
+    });
+    console.log(`Token response: ${JSON.stringify(tokenResponse.data)}`);
+
+    res.json(tokenResponse.data);
+});
+
+/* exchange_public_token process
+ involves exchanging a public_token for 
+an access_token using the
+ /item/public_token/exchange endpoint. */
+app.post('/api/exchange_public_token', async(req, res, next)=>{
+    const exchangeResponse = await client.itemPublicTokenExchange({
+        public_token: req.body.public_token,
+});
+
+
+console.log(`Response:  ${JSON.stringify(exchangeResponse.data)}`)
+req.session.access_token = exchangeResponse.data.access_token;
+res.json(true);
+});
+
+app.get('/api/is_user_connected', async (req, res, next) => {
+    console.log('Access Token: ', req.session.access_token);
+    return req.session.access_token ? res.json({ status : true}) : res.json({ status : false});
+})
+
+
+app.get('/api/get_connected_bank', async(req, res, next)=>{
+    accessToken = req.session.access_token;
+    const bankRequest = await client.itemGet({
+        access_token: accessToken
+    })
+    console.log(`Item Response:  ${JSON.stringify(bankRequest.data)}`);
+    const bankId = bankRequest.data.item.institution_id;
+    const nameRequest = await client.institutionsGetById({
+        institution_id: bankId,
+        country_codes: ["US"]
+    });
+    console.log(`ID Response:  ${JSON.stringify(nameRequest.data)}`);
+    res.json(nameRequest.data.institution.name);
+});
+
+//
+app.get('/api/get_accounts', async (req, res, next) =>{
+    const getAccounts = await client.accountsGet({
+        access_token: req.session.access_token
+    });
+    console.log(getAccounts.data);
+    res.json(getAccounts.data.accounts);
+});
+
+app.get('/api/get_transactions', async (req, res, next) =>{
+    const getTransactions = await client.transactionsGet({
+        access_token: req.session.access_token,
+        start_date: '2024-10-01',
+        end_date: '2024-10-10'
+    });
+    console.log(getTransactions.data);
+    res.json(getTransactions.data.transactions);
+});
+
